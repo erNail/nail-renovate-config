de382456c15fe87cf8d80c2b2f2385518284b25f
Eric Nagel <nagel.eric.95@googlemail.com>
2023-06-18 18:13:01 +0200

    feat(renovate-config): Add renovate configuration

---
+++ renovate-config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  platform: 'github',
+  branchPrefix: 'dependency-update/',
+  semanticCommitScope: 'dependencies',
+  commitMessageLowerCase: 'never',
+  username: 'renovate-bot',
+  gitAuthor: 'Renovate Bot <bot@renovateapp.com>',
+  onboarding: true,
+  labels: ["Renovate"],
+  repositories: [
+    "erNail/erNail.github.io",
+    "erNail/nail-renovate-config"
+  ],
+};
